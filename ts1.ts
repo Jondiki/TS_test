ae4747970c82234ebb123e72a94fbd3584fe1933
Jonathan DIki <jonathandiki@outlook.com>
2024-06-25 10:06:29 +0200

    delete tsx

--- ts1.ts
+++ ts1.ts
@@ -1,6 +1,6 @@
-function addition(x: number, y: number): number {
+function addition(x:number, y: number): number {
     return x + y;
 }
 
-const result: number = addition('10', '20');
+const result: number = addition(10, 20);
 console.log(result);
